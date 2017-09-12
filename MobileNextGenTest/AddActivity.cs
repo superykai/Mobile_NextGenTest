12f13fcdbe748a3b7c0bfb9f1a76d379898a2242
Kai Yang <SZWRLY@NAM.corp.gm.com>
2017-09-11 21:16:06 -0700

    Add project files.

---
+++ MobileNextGenTest/AddActivity.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.IO;
+using System.Json;
+using System.Linq;
+using System.Net;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+using System.Threading.Tasks;
+using Android.App;
+using Android.Content;
+using Android.Graphics;
+using Android.Graphics.Drawables;
+using Android.Media;
+using Android.OS;
+using Android.Widget;
+using Android_app1;
+using Java.IO;
+using Newtonsoft.Json;
+using Console = System.Console;
+using Context = System.Runtime.Remoting.Contexts.Context;
+using Encoding = System.Text.Encoding;
+
+namespace firstAndroidApp
+{
+    [Activity(Label = "Add New Test")]
+    public class AddActivity: Activity
+    {
+        //public static readonly int PickImageId = 1000;
+        //private ImageView _imageView;
+        private Android.Net.Uri _uri;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "addNew" layout resource 
+            SetContentView(Resource.Layout.addNew);
+
+            TextView addrequId = FindViewById<TextView>(Resource.Id.AddTxtReqNum);
+            TextView addmodelyear = FindViewById<TextView>(Resource.Id.AddTxtModelYear);
+            TextView partNum = FindViewById<TextView>(Resource.Id.AddTxtPartNum);
+            TextView productCode = FindViewById<TextView>(Resource.Id.AddTxtProductCode);
+            TextView vehProgram = FindViewById<TextView>(Resource.Id.AddTxtvehProgram);
+            TextView vehArchitecture = FindViewById<TextView>(Resource.Id.AddTxtvehArchitecture);
+            TextView vehMake = FindViewById<TextView>(Resource.Id.AddTxtvehMake);
+            TextView upc = FindViewById<TextView>(Resource.Id.AddTxtupc);
+            TextView vppsLevel1 = FindViewById<TextView>(Resource.Id.AddTxtvppsLevel1); 
+            TextView functionalArea = FindViewById<TextView>(Resource.Id.AddTxtfunctionalArea);
+            TextView createdByGmin = FindViewById<TextView>(Resource.Id.AddTxtcreatedByGmin);
+            TextView createdDate = FindViewById<TextView>(Resource.Id.AddTxtcreatedDate);
+            TextView updatedDate = FindViewById<TextView>(Resource.Id.AddTxtupdatedDate);
+
+            Button button = FindViewById<Button>(Resource.Id.myButton);
+            
+            //_imageView.SetImageResource(Resource.Drawable.allFood);
+
+            button.Click += (sender, e) =>
+            {
+
+                string url = "https://test-request-service.apps.pcfepgwi.gm.com/testRequests";
+
+                NewTest test = new NewTest();
+                test.requestNum = int.Parse(addrequId.Text);
+                test.modelYear = addmodelyear.Text;
+                test.partNum = partNum.Text;
+                test.productCode = productCode.Text;
+                test.vehProgram = vehProgram.Text;
+                test.vehArchitecture = vehArchitecture.Text;
+                test.vehMake = vehMake.Text;
+                test.upc = upc.Text;
+                test.vppsLevel1 = vppsLevel1.Text;
+                test.functionalArea = functionalArea.Text;
+                test.createdByGmin = createdByGmin.Text;
+                test.createdDate = createdDate.Text;
+                test.updatedDate = updatedDate.Text;
+
+                //string data = JsonConvert.SerializeObject(test);
+                //byte[] bytes;
+
+                //Bitmap bitmap = Android.Provider.MediaStore.Images.Media.GetBitmap(ContentResolver, _uri);
+                //using (var stream = new MemoryStream())
+                //{
+
+                //    bitmap.Compress(Bitmap.CompressFormat.Jpeg, 100, stream);
+                //    bytes = stream.ToArray();
+                //}
+                ////Binary bit= new Binary();
+                //picture bit = new picture();
+                //bit.Bytes = bytes;
+
+                //Category newcCategory = new Category();
+                //newcCategory.CategoryName = addName.Text;
+                //newcCategory.Description = addDescription.Text;
+                //newcCategory.Picture = bit;
+
+                ////string content = JsonConvert.SerializeObject(newcCategory);
+                //DataContractJsonSerializer ser = new DataContractJsonSerializer(typeof(Category)); 
+                //MemoryStream mem = new MemoryStream();
+                //ser.WriteObject(mem, newcCategory);
+                //string data = Encoding.UTF8.GetString(mem.ToArray(), 0, (int)mem.Length);
+
+                DataContractJsonSerializer ser = new DataContractJsonSerializer(typeof(NewTest));
+                MemoryStream mem = new MemoryStream();
+                ser.WriteObject(mem, test);
+                string data = Encoding.UTF8.GetString(mem.ToArray(), 0, (int)mem.Length);
+
+                using (WebClient client = new WebClient())
+                {
+                    try
+                    {
+                        client.Headers["Content-type"] = "application/json";
+                        //client.Encoding = Encoding.UTF8;
+                        client.UploadString(url, "POST", data);
+                        Toast.MakeText(ApplicationContext,"Your new test successfully added!",ToastLength.Long).Show();
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.Out.WriteLine("Error: {0}", ex.Message);
+                    }
+                }
+            };
+            
+        }
+
+        //private void ButtonOnClick(object sender, EventArgs eventArgs)
+        //{
+        //    Intent = new Intent();
+        //    Intent.SetType("image/*");
+        //    Intent.SetAction(Intent.ActionGetContent);
+        //    StartActivityForResult(Intent.CreateChooser(Intent, "Select Picture"), PickImageId);
+        //}
+
+        //protected override void OnActivityResult(int requestCode, Result resultCode, Intent data)
+        //{
+        //    if ((requestCode == PickImageId) && (resultCode == Result.Ok) && (data != null))
+        //    {
+        //        _uri = data.Data;
+        //        _imageView.SetImageURI(_uri);
+        //    }
+        //}
+    }
+
+    public class NewTest
+    {
+        public int requestNum { get; set; }
+        public string modelYear { get; set; }
+        public string partNum { get; set; }
+        public string productCode { get; set; }
+        public string vehProgram { get; set; }
+        public string vehArchitecture { get; set; }
+        public string vehMake { get; set; }
+        public string upc { get; set; }
+        public string vppsLevel1 { get; set; }
+        public string functionalArea { get; set; }
+        public string createdByGmin { get; set; }
+        public string createdDate { get; set; }
+        public string updatedDate { get; set; }
+    }
+   
+}
---
+++ MobileNextGenTest/MainActivity.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using System.Net;
+using System.IO;
+using System.Json;
+using System.Threading.Tasks;
+using Android.Graphics;
+using System.Linq;
+using Android_app1;
+
+
+namespace firstAndroidApp
+{
+    [Activity(Label = "MobileNextGenTest", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource 
+            SetContentView(Resource.Layout.Main);
+
+            // Get the latitude/longitude EditBox and button resources:
+            EditText requestnum = FindViewById<EditText>(Resource.Id.requestText);
+            Button button = FindViewById<Button>(Resource.Id.getGeoButton);
+            Button button1 = FindViewById<Button>(Resource.Id.button1);
+
+            // When the user clicks the button ...
+            button.Click += async (sender, e) => {
+
+               
+                //string url = "http://10.0.2.2:9002/SampleServices/Service/REST/Category/"+ Country.Text;
+                string url =
+                    "https://test-request-service.apps.pcfepgwi.gm.com/testRequests/" + requestnum.Text;
+
+
+                JsonValue json = await FetchGEOAsync(url);
+                ParseAndDisplay(json);
+            };
+
+            button1.Click +=  delegate {
+                StartActivity(typeof(AddActivity));
+            };
+        }
+
+        // Gets weather data from the passed URL.
+        private async Task<JsonValue> FetchGEOAsync(string url)
+        {
+            // Create an HTTP web request using the URL:
+            HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create(new Uri(url));
+            request.ContentType = "application/json";
+            request.Method = "GET";
+
+            WebResponse response =  request.GetResponse();
+            Stream stream = response.GetResponseStream();
+            JsonValue jsonDoc = JsonObject.Load(stream);
+            Console.Out.WriteLine("Response: {0}", jsonDoc.ToString());
+            return jsonDoc;
+
+            // Send the request to the server and wait for the response:
+            //using (WebResponse response = await request.GetResponseAsync())
+            //{
+            //    // Get a stream representation of the HTTP web response:
+            //    using (Stream stream = response.GetResponseStream())
+            //    {
+            //        // Use this stream to build a JSON document object:
+            //        JsonValue jsonDoc = await Task.Run(() => JsonObject.Load(stream));
+            //        Console.Out.WriteLine("Response: {0}", jsonDoc.ToString());
+
+            //        // Return the JSON document:
+            //        return jsonDoc;
+            //    }
+            //}
+        }
+
+        // Parses the weather data, then writes temperature, humidity, conditions, and 
+        // location to the screen.
+        private void ParseAndDisplay(JsonValue json)
+        {
+            // Get the weather reporting fields from the layout resource: 
+            TextView ModelYear = FindViewById<TextView>(Resource.Id.resultModelYearText);
+            TextView PartNumber = FindViewById<TextView>(Resource.Id.resultpartText);
+            TextView ProductCode = FindViewById<TextView>(Resource.Id.resultproductCodeText);
+            TextView VehProg = FindViewById<TextView>(Resource.Id.resultvehProgText);
+            TextView VehArchit = FindViewById<TextView>(Resource.Id.resultvehArchitText);
+            TextView VehMake = FindViewById<TextView>(Resource.Id.resultvehmakeText);
+            TextView upc = FindViewById<TextView>(Resource.Id.resultupcText);
+            TextView vppsLevel1 = FindViewById<TextView>(Resource.Id.resultvppsLevel1Text);
+            TextView functionalArea = FindViewById<TextView>(Resource.Id.resultfunctionalAreaText);
+            TextView createdByGmin = FindViewById<TextView>(Resource.Id.resultcreatedByGminText);
+            TextView createdDate = FindViewById<TextView>(Resource.Id.resultcreatedDateText);
+            TextView updatedDate = FindViewById<TextView>(Resource.Id.resultupdatedDateText);
+            //ImageView image = FindViewById<ImageView>(Resource.Id.imageView1);
+
+            // Extract the array of name/value results for the field name "weatherObservation":
+            // Note that there is no exception handling for when this field is not found.
+            JsonValue mResults = json;
+
+            
+            //string mm = json["Picture"]["Bytes"].ToString();
+            //Byte[] picData = mm.Trim('[', ']')
+            //  .Split(',')
+            //  .Select(x => byte.Parse(x))
+            //  .ToArray();
+
+            //int offSet = 0;
+            //if (picData[0].ToString().Equals("21"))
+            //    offSet = 78;
+            //Bitmap bmp = BitmapFactory.DecodeByteArray(picData, offSet, picData.Length - offSet);
+
+            //image.SetImageBitmap(Bitmap.CreateScaledBitmap(bmp, image.Width, image.Height, false));
+
+
+            // Draw your image
+            //Bitmap bmp = Bitmap.CreateBitmap(200, 200, Bitmap.Config.Argb8888);
+            //image.SetImageBitmap(Bitmap.CreateScaledBitmap(bmp, image.Width, image.Height, false));
+
+
+            // Extract the "stationName" (location string) and write it to the location TextBox:
+            ModelYear.Text = mResults["modelYear"].ToString();
+            PartNumber.Text = mResults["partNum"];
+            ProductCode.Text = mResults["productCode"];
+            VehProg.Text = mResults["vehProgram"];
+            VehArchit.Text = mResults["vehArchitecture"];
+            VehMake.Text = mResults["vehMake"];
+            upc.Text = mResults["upc"];
+            vppsLevel1.Text = mResults["vppsLevel1"];
+            functionalArea.Text = mResults["functionalArea"];
+            createdByGmin.Text = mResults["createdByGmin"];
+            createdDate.Text = mResults["createdDate"];
+            updatedDate.Text = mResults["updatedDate"];
+            //capitol.Text = mResults["capital"];
+
+
+        }
+    }
+}
+
